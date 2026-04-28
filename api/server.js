acb96c7aadd244597e360fc764b0790b8b5031a2
RADA1807 <robert1807@hotmail.com>
2026-04-28 10:53:59 -0600

    fix: actualizar CORS para permitir uploads desde localhost

--- api/server.js
+++ api/server.js
@@ -10,11 +10,13 @@ app.use(cors({
   origin: [
     process.env.FRONTEND_URL,
     'http://localhost:3000',
-    'http://localhost:3001',
+    'http://localhost:8081',
+    'http://localhost:19006',
   ],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.use(express.json());
 
 // Importar middlewares
 const verifyToken = require('./middlewares/verifyToken');
